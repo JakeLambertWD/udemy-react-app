da22e24396277baf66bbe5972390c6727d7b66e2
Jake Lambert <jakelambert1@hotmail.co.uk>
2020-06-01 15:29:55 +0100

    Section 4: Working w/ lists & conditionals

--- src/App.js
+++ src/App.js
@@ -1,85 +1,110 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-// react functional component 
-const app = props => {
-  // useState is a hook that allows us to manage state in a funcitonal component  
-  // [] array destructuring which allows you to pull elements out of the array
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
-    ]
-  });
-
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  // Handle event with a function
-  const switchNameHandler = (newName) => {
-    // in this case, will merge this new state with the old state
-    setPersonsState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
-  };
-
-  const nameChangedHandler = (event) => {
-    setPersonsState({
-      persons: [
-        { name: 'Max', age: 28 },
-        // target = input element & value = value of the target
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
+      { id: 1, name: 'Max', age: 28 },
+      { id: 2, name: 'Manu', age: 29 },
+      { id: 3, name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
   }
 
-    // inline styles. writing css in JS
+
+  nameChangedHandler = ( event, id ) => {
+    // The findIndex() method executes the callback function once for every index in the array 
+    // until it finds the one where callback returns a truthly value.
+    // If such an element is found, findIndex() immediately returns the element's index.
+
+    // return the state objects index that matches the changed name id 
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // get the object in the persons array by its index
+    const personObj = {
+      // Spread operator
+      ...this.state.persons[personIndex]
+    };
+
+    // change the name to the contents of the input field
+    personObj.name = event.target.value;
+
+    // get a copy of the persons array
+    const persons = [...this.state.persons];
+    // update object 
+    persons[personIndex] = personObj;
+    // update the state array to the updated array
+    this.setState( {persons: persons} );
+  }
+
+
+  deletePersonsHandler = (personIndex) => {
+    // the spread operator makes a copy of the persons array
+    const persons = [...this.state.persons];
+    // removes the 1 element from array according to its index
+    persons.splice(personIndex, 1);
+    // update state
+    this.setState({persons: persons});
+  }
+
+
+  togglePersonsHandler = () => {
+    // store boolean as variable 
+    const doesShow = this.state.showPersons;
+    // reverse the boolean 
+    this.setState({showPersons: !doesShow})
+  }
+
+
+  render () { 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
-      border: '1px solid red',
+      border: '2px solid black',
       padding: '8px',
       cursor: 'pointer'
     };
 
-  return (
-    // JSX 
-    // className translates to class in HTML
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      {/* we insert an arrow function so we can parse something */}
-      <button 
-      style={style} 
-      onClick={() => switchNameHandler('auba')}>Switch Name</button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-        // parse handler as a property
-        click={switchNameHandler}
-        changed={nameChangedHandler}>
-        My Hobbies: Racing
-      </Person>
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
-    </div>
-  );
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-};
+    // declare the variable
+    let persons = null;
+    // if showPersons is true
+    if ( this.state.showPersons ) {
+      persons = (
+        // JSX
+        <div>
+          {/* the map method loops through the persons array */}
+          {/* for every item in the array, it gives the item itself in person*/}
+          {/* and the postion of the item in index */}
+          {this.state.persons.map((person, index) => {
+              // Call Person.js 
+              return <Person 
+              key={person.id}
+              name={person.name} 
+              age={person.age}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+              click={() => this.deletePersonsHandler(index)}
+              />
+          })}
+          </div>
+      );
+    }
 
-// export app component
-export default app;
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle persons</button>
+        {/* refer to the persons variable */}
+        {persons}
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import './Person.css';
 
-// function
-// props parses all the attributes we added to the component
 const person = ( props ) => {
-    // outputting dynamic content
     return (
-        <div className="Person">
+        <div>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            {/*children any elements between opening and closing tag of our component*/}
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed}  value={props.name}/>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 };
-
+ 
 export default person;
